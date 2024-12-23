9d7292d4d9d106eec957f53c510b716861919595
madurangaPrabhath <maduracourse@gmail.com>
2024-12-23 13:18:21 +0530

    Edit App.js file

--- MyMobileApp/App.js
+++ MyMobileApp/App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { PaperProvider } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { PaperProvider, Text, Divider } from 'react-native-paper';
 
 export default function App() {
   return (
     <PaperProvider>
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text variant="headlineLarge">Headline Large</Text>
+        <Divider />
+        <Text variant="bodyMedium" style={styles.body}>
+          If a dedicated prop for a specific color is not available or the style prop does not allow color modification, you can customize it using the theme prop. It allows to override any color, within the component, based on the table above.
+        </Text>
         <StatusBar style="auto" />
       </View>
     </PaperProvider>
@@ -20,4 +24,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  body:{
+    padding: 8,
+    textAlign: 'justify'
+  }
 });
